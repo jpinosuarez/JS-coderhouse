f4a5166facb253c8c8c304bca898062a0b38883d
jpinosuarez <pinosuarezjoaquin@gmail.com>
2022-01-25 18:55:39 -0300

    clase dom

---
+++ js/buscador.js
@@ -0,0 +1,203 @@
+// Objetos
+
+class Habitacion {
+    constructor (nombre,descripcion,info,precio, tipo){
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+        this.info=info;
+        this.precio=precio;
+        this.tipo=tipo;
+    }
+    sumarIva(){
+        this.precio=this.precio*1.21;
+    }
+    mostrarDatos(){
+        console.log("La "+this.nombre+" tiene una capacidad de "+this.capacidad+" persona/s, y cuesta $"+this.precio+" por día (IVA incluido).");
+    }
+}
+
+const habitacionSimpleClasica = new Habitacion ("Habitación Simple Clásica","descripcion","info",5000,"simpleClasica");
+const habitacionDobleClasica = new Habitacion ("Habitación Doble Clásica","descripcion","info",6000,"dobleClasica");
+const habitacionCuadrupleClasica = new Habitacion ("Habitacion Cuádruple Clásica","descripcion","info",7000,"cuadrupleClasica");
+const habitacionFamiliarClasica = new Habitacion ("Habitación Familiar Clásica","descripcion","info",8000,"familiarClasica");
+
+const habitacionSimpleMaster = new Habitacion ("Habitación Simple Master","descripcion","info",5500,"simpleMaster");
+const habitacionDobleMaster = new Habitacion ("Habitación Doble Master","descripcion","info",6500,"dobleMaster");
+const habitacionCuadrupleMaster = new Habitacion ("Habitacion Cuádruple Master","descripcion","info",7500,"cuadrupleMaster");
+const habitacionFamiliarMaster = new Habitacion ("Habitación Familiar Master","descripcion","info",8500,"familiarMaster");
+
+const habitacionSimpleDeluxe = new Habitacion ("Habitación Simple Deluxe","descripcion","info",5500,"simpleDeluxe");
+const habitacionDobleDeluxe = new Habitacion ("Habitación Doble Deluxe","descripcion","info",6500,"dobleDeluxe");
+const habitacionCuadrupleDeluxe = new Habitacion ("Habitacion Cuádruple Deluxe","descripcion","info",7500,"cuadrupleDeluxe");
+const habitacionFamiliarDeluxe = new Habitacion ("Habitación Familiar Deluxe","descripcion","info",8500,"familiarDeluxe");
+
+
+// arrays habitaciones por tipo
+
+const habitacionesSimples = [habitacionSimpleClasica, habitacionSimpleMaster, habitacionSimpleDeluxe];
+const habitacionesDobles = [habitacionDobleClasica, habitacionDobleMaster, habitacionDobleDeluxe];
+const habitacionesCuadruples = [habitacionCuadrupleClasica, habitacionCuadrupleMaster, habitacionCuadrupleDeluxe];
+const habitacionesFamiliares = [habitacionFamiliarClasica, habitacionFamiliarMaster, habitacionFamiliarDeluxe];
+
+console.log(habitacionesSimples);
+console.log(habitacionesDobles);
+console.log(habitacionesCuadruples);
+console.log(habitacionesFamiliares);
+
+
+// ===========================
+
+let contenedorHabitaciones = document.getElementById("contenedor-habitaciones");
+
+
+// for (let index = 0; index < habitacionesSimples.length; index++) {
+//     let habitacion=document.createElement("div");
+//     habitacion.innerHTML = `<img class="img-fluid rounded img-room" src="../public/${habitacionesSimples[index].tipo}.jpg" alt="">
+//                             <div class="d-flex flex-column align-items-center w-100 p-2">
+//                                 <h3 class="text-center">${habitacionesSimples[index].nombre}</h3>
+//                                 <p>${habitacionesSimples[index].info}</p>
+//                                 <p>${habitacionesSimples[index].descripcion}</p>
+//                                 <p>$${habitacionesSimples[index].precio}</p>
+//                                 <a href="#"><button type="button" class="btn btn-primary">SELECCIONAR</button></a>
+//                             </div>`; 
+//     habitacion.className = ("d-flex");
+//     habitacion.classList.add("border","flex-row","mb-3","rounded")
+//     contenedorHabitaciones.appendChild(habitacion);
+    
+// }
+
+// for (let index = 0; index < habitacionesDobles.length; index++) {
+//     let habitacion=document.createElement("div");
+//     habitacion.innerHTML = `<img class="img-fluid rounded img-room" src="../public/${habitacionesDobles[index].tipo}.jpg" alt="">
+//                             <div class="d-flex flex-column align-items-center w-100 p-2">
+//                                 <h3 class="text-center">${habitacionesDobles[index].nombre}</h3>
+//                                 <p>${habitacionesDobles[index].info}</p>
+//                                 <p>${habitacionesDobles[index].descripcion}</p>
+//                                 <p>$${habitacionesDobles[index].precio}</p>
+//                                 <a href="#"><button type="button" class="btn btn-primary">SELECCIONAR</button></a>
+//                             </div>`; 
+//     habitacion.className = ("border");
+//     habitacion.classList.add("d-flex","flex-row","mb-3","rounded")
+//     contenedorHabitaciones.appendChild(habitacion);
+    
+// }
+
+// for (let index = 0; index < habitacionesCuadruples.length; index++) {
+//     let habitacion=document.createElement("div");
+//     habitacion.innerHTML = `<img class="img-fluid rounded img-room" src="../public/${habitacionesCuadruples[index].tipo}.jpg" alt="">
+//                             <div class="d-flex flex-column align-items-center w-100 p-2">
+//                                 <h3 class="text-center">${habitacionesCuadruples[index].nombre}</h3>
+//                                 <p>${habitacionesCuadruples[index].info}</p>
+//                                 <p>${habitacionesCuadruples[index].descripcion}</p>
+//                                 <p>$${habitacionesCuadruples[index].precio}</p>
+//                                 <a href="#"><button type="button" class="btn btn-primary">SELECCIONAR</button></a>
+//                             </div>`; 
+//     habitacion.className = ("border");
+//     habitacion.classList.add("d-flex","flex-row","mb-3","rounded")
+//     contenedorHabitaciones.appendChild(habitacion);
+// }
+
+// for (let index = 0; index < habitacionesFamiliares.length; index++) {
+//     let habitacion=document.createElement("div");
+//     habitacion.innerHTML = `<img class="img-fluid rounded img-room" src="../public/${habitacionesFamiliares[index].tipo}.jpg" alt="">
+//                             <div class="d-flex flex-column align-items-center w-100 p-2">
+//                                 <h3 class="text-center">${habitacionesFamiliares[index].nombre}</h3>
+//                                 <p>${habitacionesFamiliares[index].info}</p>
+//                                 <p>${habitacionesFamiliares[index].descripcion}</p>
+//                                 <p>$${habitacionesFamiliares[index].precio}</p>
+//                                 <a href="#"><button type="button" class="btn btn-primary">SELECCIONAR</button></a>
+//                             </div>`; 
+//     habitacion.className = ("border");
+//     habitacion.classList.add("d-flex","flex-row","mb-3","rounded")
+//     contenedorHabitaciones.appendChild(habitacion);
+// }
+
+// =======================
+
+let divDias = document.getElementById('dias');
+let cantDias = document.createElement('p');
+cantDias.innerHTML = localStorage.getItem('dias');
+divDias.appendChild(cantDias);
+
+let divAdultos = document.getElementById('adultos');
+let cantAdultos = document.createElement('p');
+cantAdultos.innerHTML = localStorage.getItem('adultos');
+divAdultos.appendChild(cantAdultos);
+
+let divMenores = document.getElementById('menores');
+let cantMenores = document.createElement('p');
+cantMenores.innerHTML = localStorage.getItem('menores');
+divMenores.appendChild(cantMenores);
+
+// ==========================
+
+let huespedes = parseInt(localStorage.getItem('adultos'))+parseInt(localStorage.getItem('menores'));
+
+if (huespedes==1) {
+    for (let index = 0; index < habitacionesSimples.length; index++) {
+    let habitacion=document.createElement("div");
+    habitacion.innerHTML = `<img class="img-fluid rounded img-room" src="../public/${habitacionesSimples[index].tipo}.jpg" alt="">
+                            <div class="d-flex flex-column align-items-center w-100 p-2">
+                                <h3 class="text-center">${habitacionesSimples[index].nombre}</h3>
+                                <p>${habitacionesSimples[index].info}</p>
+                                <p>${habitacionesSimples[index].descripcion}</p>
+                                <p>$${habitacionesSimples[index].precio}</p>
+                                <a href="#"><button type="button" class="btn btn-primary">SELECCIONAR</button></a>
+                            </div>`; 
+    habitacion.className = ("d-flex");
+    habitacion.classList.add("border","flex-row","mb-3","rounded")
+    contenedorHabitaciones.appendChild(habitacion);
+    }
+} else if (huespedes==2) {
+    for (let index = 0; index < habitacionesDobles.length; index++) {
+    let habitacion=document.createElement("div");
+    habitacion.innerHTML = `<img class="img-fluid rounded img-room" src="../public/${habitacionesDobles[index].tipo}.jpg" alt="">
+                            <div class="d-flex flex-column align-items-center w-100 p-2">
+                                <h3 class="text-center">${habitacionesDobles[index].nombre}</h3>
+                                <p>${habitacionesDobles[index].info}</p>
+                                <p>${habitacionesDobles[index].descripcion}</p>
+                                <p>$${habitacionesDobles[index].precio}</p>
+                                <a href="#"><button type="button" class="btn btn-primary">SELECCIONAR</button></a>
+                            </div>`; 
+    habitacion.className = ("border");
+    habitacion.classList.add("d-flex","flex-row","mb-3","rounded")
+    contenedorHabitaciones.appendChild(habitacion);
+    }
+}else if ((huespedes==3)||(huespedes==4)) {
+    for (let index = 0; index < habitacionesCuadruples.length; index++) {
+    let habitacion=document.createElement("div");
+    habitacion.innerHTML = `<img class="img-fluid rounded img-room" src="../public/${habitacionesCuadruples[index].tipo}.jpg" alt="">
+                            <div class="d-flex flex-column align-items-center w-100 p-2">
+                                <h3 class="text-center">${habitacionesCuadruples[index].nombre}</h3>
+                                <p>${habitacionesCuadruples[index].info}</p>
+                                <p>${habitacionesCuadruples[index].descripcion}</p>
+                                <p>$${habitacionesCuadruples[index].precio}</p>
+                                <a href="#"><button type="button" class="btn btn-primary">SELECCIONAR</button></a>
+                            </div>`; 
+    habitacion.className = ("border");
+    habitacion.classList.add("d-flex","flex-row","mb-3","rounded")
+    contenedorHabitaciones.appendChild(habitacion);}
+} else if ((huespedes>4)&&(huespedes<=6)) {
+    for (let index = 0; index < habitacionesFamiliares.length; index++) {
+    let habitacion=document.createElement("div");
+    habitacion.innerHTML = `<img class="img-fluid rounded img-room" src="../public/${habitacionesFamiliares[index].tipo}.jpg" alt="">
+                            <div class="d-flex flex-column align-items-center w-100 p-2">
+                                <h3 class="text-center">${habitacionesFamiliares[index].nombre}</h3>
+                                <p>${habitacionesFamiliares[index].info}</p>
+                                <p>${habitacionesFamiliares[index].descripcion}</p>
+                                <p>$${habitacionesFamiliares[index].precio}</p>
+                                <a href="#"><button type="button" class="btn btn-primary">SELECCIONAR</button></a>
+                            </div>`; 
+    habitacion.className = ("border");
+    habitacion.classList.add("d-flex","flex-row","mb-3","rounded")
+    contenedorHabitaciones.appendChild(habitacion);}
+}else {
+    let mensaje=document.createElement("div");
+    mensaje.innerHTML = `No hay habitaciones disponibles`; 
+    mensaje.className = ("border");
+    mensaje.classList.add("d-flex","flex-row","mb-3","rounded")
+    contenedorHabitaciones.appendChild(mensaje);
+}
+
+
+console.log("huespedes: "+huespedes);
--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // Desafío 5 - Objetos
 
 class Habitacion {
-    constructor (nombre,capacidad,precioPorDia){
+    constructor (nombre,capacidad,precio, tipo){
         this.nombre=nombre;
         this.capacidad=capacidad;
-        this.precio=precioPorDia;
+        this.precio=precio;
+        this.tipo=tipo;
     }
     sumarIva(){
         this.precio=this.precio*1.21;
@@ -14,30 +15,50 @@ class Habitacion {
     }
 }
 
-const habitacionSimple = new Habitacion ("Habitación Simple",1,5000);
-const habitacionDoble = new Habitacion ("Habitación Doble",2,6000);
-const habitacionCuadruple = new Habitacion ("Habitacion Cuádruple",4,7000);
-const habitacionFamiliar = new Habitacion ("Habitación Familiar",6,8000);
+const habitacionSimpleClasica = new Habitacion ("Habitación Simple",1,5000,"Clásica");
+const habitacionDobleClasica = new Habitacion ("Habitación Doble",2,6000,"Clásica");
+const habitacionCuadrupleClasica = new Habitacion ("Habitacion Cuádruple",4,7000,"Clásica");
+const habitacionFamiliarClasica = new Habitacion ("Habitación Familiar",6,8000,"Clásica");
+
+const habitacionSimpleMaster = new Habitacion ("Habitación Simple",1,5500,"Master");
+const habitacionDobleMaster = new Habitacion ("Habitación Doble",2,6500,"Master");
+const habitacionCuadrupleMaster = new Habitacion ("Habitacion Cuádruple",4,7500,"Master");
+const habitacionFamiliarMaster = new Habitacion ("Habitación Familiar",6,8500,"Master");
+
+const habitacionSimpleDeluxe = new Habitacion ("Habitación Simple",1,5500,"Deluxe");
+const habitacionDobleDeluxe = new Habitacion ("Habitación Doble",2,6500,"Deluxe");
+const habitacionCuadrupleDeluxe = new Habitacion ("Habitacion Cuádruple",4,7500,"Deluxe");
+const habitacionFamiliarDeluxe = new Habitacion ("Habitación Familiar",6,8500,"Deluxe");
 
 
 // Desafio 6 - Arrays
 
-const listaHabitaciones = [];
+const listaHabitacionesClasicas = [];
 
-listaHabitaciones.push (habitacionSimple, habitacionDoble, habitacionCuadruple, habitacionFamiliar);
+listaHabitacionesClasicas.push (habitacionSimpleClasica, habitacionDobleClasica, habitacionCuadrupleClasica, habitacionFamiliarClasica);
 
-console.log(listaHabitaciones);
+const listaHabitacionesMaster = [];
 
-habitacionSimple.sumarIva();
-habitacionDoble.sumarIva();
-habitacionCuadruple.sumarIva();
-habitacionFamiliar.sumarIva();
+listaHabitacionesMaster.push (habitacionSimpleMaster, habitacionDobleMaster, habitacionCuadrupleMaster, habitacionFamiliarMaster);
+
+const listaHabitacionesDeluxe = [];
+
+listaHabitacionesDeluxe.push (habitacionSimpleDeluxe, habitacionDobleDeluxe, habitacionCuadrupleDeluxe, habitacionFamiliarDeluxe);
+
+console.log(listaHabitacionesClasicas);
+console.log(listaHabitacionesMaster);
+console.log(listaHabitacionesDeluxe);
+
+// habitacionSimple.sumarIva();
+// habitacionDoble.sumarIva();
+// habitacionCuadruple.sumarIva();
+// habitacionFamiliar.sumarIva();
 
 
-habitacionSimple.mostrarDatos();
-habitacionDoble.mostrarDatos();
-habitacionCuadruple.mostrarDatos();
-habitacionFamiliar.mostrarDatos();
+// habitacionSimple.mostrarDatos();
+// habitacionDoble.mostrarDatos();
+// habitacionCuadruple.mostrarDatos();
+// habitacionFamiliar.mostrarDatos();
 
 
 // ===================================
@@ -48,22 +69,42 @@ let dias = parseInt(prompt("Ingrese la cantidad de días:"));
 let huespedes = adultos+menores;
 let precio = 0;
 
-function calcularPrecio(){
-    if (huespedes==1) {
-        precio = habitacionSimple.precio*dias;
-        alert("Su estadía de "+dias+" dias, para "+huespedes+" persona, en una habitación simple, es de $"+precio+".");
-    } else if (huespedes==2) {
-        precio = habitacionDoble.precio*dias;
-        alert("Su estadía de "+dias+" dias, para "+huespedes+" personas, en una habitación doble, es de $"+precio+".");
-    }else if ((huespedes==3)||(huespedes==4)) {
-        precio = habitacionCuadruple.precio*dias;
-        alert("Su estadía de "+dias+" dias, para "+huespedes+" personas, en una habitación cuádruple, es de $"+precio+".");
-    }else if ((huespedes==5)||(huespedes==6)) {
-        precio = habitacionFamiliar.precio*dias;
-        alert("Su estadía de "+dias+" dias, para "+huespedes+" personas, en una habitación familiar, es de $"+precio+".");
-    }else {
-        alert("No tenemos habitaciones disponibles.");
-    }
-}
+localStorage.setItem('dias',dias);
+localStorage.setItem('adultos',adultos);
+localStorage.setItem('menores',menores);
 
-calcularPrecio();
+let divDias = document.getElementById('dias');
+let cantDias = document.createElement('p');
+cantDias.innerHTML = localStorage.getItem('dias');
+divDias.appendChild(cantDias);
+
+let divAdultos = document.getElementById('adultos');
+let cantAdultos = document.createElement('p');
+cantAdultos.innerHTML = localStorage.getItem('adultos');
+divAdultos.appendChild(cantAdultos);
+
+let divMenores = document.getElementById('menores');
+let cantMenores = document.createElement('p');
+cantMenores.innerHTML = localStorage.getItem('menores');
+divMenores.appendChild(cantMenores);
+
+
+// function calcularPrecio(){
+//     if (huespedes==1) {
+//         precio = habitacionSimple.precio*dias;
+//         alert("Su estadía de "+dias+" dias, para "+huespedes+" persona, en una habitación simple, es de $"+precio+".");
+//     } else if (huespedes==2) {
+//         precio = habitacionDoble.precio*dias;
+//         alert("Su estadía de "+dias+" dias, para "+huespedes+" personas, en una habitación doble, es de $"+precio+".");
+//     }else if ((huespedes==3)||(huespedes==4)) {
+//         precio = habitacionCuadruple.precio*dias;
+//         alert("Su estadía de "+dias+" dias, para "+huespedes+" personas, en una habitación cuádruple, es de $"+precio+".");
+//     }else if ((huespedes==5)||(huespedes==6)) {
+//         precio = habitacionFamiliar.precio*dias;
+//         alert("Su estadía de "+dias+" dias, para "+huespedes+" personas, en una habitación familiar, es de $"+precio+".");
+//     }else {
+//         alert("No tenemos habitaciones disponibles.");
+//     }
+// }
+
+// calcularPrecio();
