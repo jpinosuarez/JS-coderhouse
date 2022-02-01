88f3d1aa775347db869a6b5b3b35341c00e3733b
jpinosuarez <pinosuarezjoaquin@gmail.com>
2022-02-01 20:02:18 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -13,14 +13,14 @@ btnSumaAdultos.addEventListener('click', sumarAdultos);
 let btnRestaAdultos = document.getElementById('restaAdultos');
 btnRestaAdultos.addEventListener('click', restarAdultos)
 
-function sumarAdultos(){
-    if ((contadorAdultos.innerHTML)<6) {
+function sumarAdultos() {
+    if ((contadorAdultos.innerHTML) < 6) {
         contadorAdultos.innerHTML++;
     }
 }
 
-function restarAdultos(){
-    if ((contadorAdultos.innerHTML)>1) {
+function restarAdultos() {
+    if ((contadorAdultos.innerHTML) > 1) {
         contadorAdultos.innerHTML--;
     }
 }
@@ -38,14 +38,14 @@ btnSumaMenores.addEventListener('click', sumarMenores);
 let btnRestaMenores = document.getElementById('restaMenores');
 btnRestaMenores.addEventListener('click', restarMenores)
 
-function sumarMenores(){
-    if ((contadorMenores.innerHTML)<6) {
+function sumarMenores() {
+    if ((contadorMenores.innerHTML) < 6) {
         contadorMenores.innerHTML++;
     }
 }
 
-function restarMenores(){
-    if ((contadorMenores.innerHTML)>0) {
+function restarMenores() {
+    if ((contadorMenores.innerHTML) > 0) {
         contadorMenores.innerHTML--;
     }
 }
@@ -54,8 +54,47 @@ function restarMenores(){
 let btnBuscar = document.getElementById('btnBuscar');
 btnBuscar.addEventListener('click', storageData);
 
-function storageData(){
+function storageData() {
     sessionStorage.setItem('adultos', contadorAdultos.innerHTML);
     sessionStorage.setItem('menores', contadorMenores.innerHTML);
     sessionStorage.setItem('dias', dias.value);
 }
+
+// Jquery UI Datepicker
+
+$("#date").datepicker();
+
+
+// ================
+
+$(function () {
+    var dateFormat = "dd/mm/yy",
+        from = $("#checkIn")
+        .datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 2
+        })
+        .on("change", function () {
+            to.datepicker("option", "minDate", getDate(this));
+        }),
+        to = $("#checkOut").datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 2
+        })
+        .on("change", function () {
+            from.datepicker("option", "maxDate", getDate(this));
+        });
+
+    function getDate(element) {
+        var date;
+        try {
+            date = $.datepicker.parseDate(dateFormat, element.value);
+        } catch (error) {
+            date = null;
+        }
+
+        return date;
+    }
+});
