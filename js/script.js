2d16c1383a54f5de857873da7fccd43898e8608b
jpinosuarez <pinosuarezjoaquin@gmail.com>
2022-02-07 20:23:01 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -54,41 +54,54 @@ function restarMenores() {
 let btnBuscar = document.getElementById('btnBuscar');
 btnBuscar.addEventListener('click', storageData);
 
-function storageData() {
-    sessionStorage.setItem('adultos', contadorAdultos.innerHTML);
-    sessionStorage.setItem('menores', contadorMenores.innerHTML);
-    sessionStorage.setItem('dias', dias.value);
-}
+// function storageData() {
+//     sessionStorage.setItem('adultos', contadorAdultos.innerHTML);
+//     sessionStorage.setItem('menores', contadorMenores.innerHTML);
+//     // sessionStorage.setItem('dias', dias.value);
+//     sessionStorage.setItem('checkIn', from.value);
+//     sessionStorage.setItem('checkOut', to.value);
+// }
 
 // Jquery UI Datepicker
 
-$("#date").datepicker();
+/*$("#date").datepicker();*/
 
 
 // ================
 
 $(function () {
-    var dateFormat = "dd/mm/yy",
+    let dateFormat = "mm/dd/yy",
         from = $("#checkIn")
         .datepicker({
+            // dateFormat: "dd-mm-yy",
             defaultDate: "+1w",
-            changeMonth: true,
-            numberOfMonths: 2
+            // changeMonth: true,
+            numberOfMonths: 2,
+            dayNames: [ "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Sábado", "Domingo" ],
+            dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
+            monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+            monthNamesShort: [ "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic" ]
         })
         .on("change", function () {
             to.datepicker("option", "minDate", getDate(this));
         }),
-        to = $("#checkOut").datepicker({
+        to = $("#checkOut")
+        .datepicker({
+            // dateFormat: "dd-mm-yy",
             defaultDate: "+1w",
-            changeMonth: true,
-            numberOfMonths: 2
+            // changeMonth: true,
+            numberOfMonths: 2,
+            dayNames: [ "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Sábado", "Domingo" ],
+            dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
+            monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+            monthNamesShort: [ "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic" ]
         })
         .on("change", function () {
             from.datepicker("option", "maxDate", getDate(this));
         });
 
     function getDate(element) {
-        var date;
+        let date;
         try {
             date = $.datepicker.parseDate(dateFormat, element.value);
         } catch (error) {
@@ -97,4 +110,50 @@ $(function () {
 
         return date;
     }
+
+    function dias() {
+        let a = $("#checkIn").datepicker('getDate').getTime(),
+            b = $("#checkOut").datepicker('getDate').getTime(),
+            c = 24*60*60*1000,
+            diffDays = Math.round(Math.abs((a - b)/(c)));
+        console.log(diffDays); //show difference
+    }
+
 });
+// ==========================
+
+function storageData() {
+    sessionStorage.setItem('adultos', contadorAdultos.innerHTML);
+    sessionStorage.setItem('menores', contadorMenores.innerHTML);
+    // sessionStorage.setItem('dias', dias.value);
+    // sessionStorage.setItem('checkIn', from);
+    // sessionStorage.setItem('checkOut', to);
+}
+
+
+
+// Calcular dias de diferencia
+
+
+
+// =============================================================
+
+// $(document).ready(function () {
+//     console.log("Ya podemos trabajar con el DOM");
+//     $('#checkIn').datepicker({
+//         // Aca va el codigo para personalizar el datepicker
+//         changeMonth: true,
+//         numberOfMonths: 2
+//     })
+
+//     $('#checkOut').datepicker({
+//         // Aca va el codigo para personalizar el datepicker
+//         changeMonth: true,
+//         numberOfMonths: 2
+//     })
+
+
+
+// })
+
+// =========================================================
